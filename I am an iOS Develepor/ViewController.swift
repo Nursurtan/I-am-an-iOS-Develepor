21308b6a964addd2e88ab179cae28561485048b5
Nursurtan <arapovnur23@gmail.com>
2021-07-17 19:02:43 +0700

    Initial Commit

---
+++ I am an iOS Develepor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am an iOS Develepor
+//
+//  Created by нурсултан арапов on 16.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
